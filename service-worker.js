982301302aac39e8e60269331cf19ccae0e94d6a
rmz38 <rmz38@cornell.edu>
2021-04-01 04:40:07 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dd-level-editor/precache-manifest.757009912020e0d6cc4afe2cda3cf848.js"
+  "/dd-level-editor/precache-manifest.74562b71af8b4cea4869e2115d9d6d39.js"
 );
 
 workbox.clientsClaim();
